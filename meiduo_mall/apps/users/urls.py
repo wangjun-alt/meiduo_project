07fa41097a1849e406d8727e08ccb1b066bd92ba
wangjun-alt <2797094441@qq.com>
2022-11-10 22:20:50 +0800

    自定义转换器converter

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -3,5 +3,5 @@ from apps.users.views import UsernameCountView
 
 urlpatterns = [
     # 判断用户名是否重复
-    path('usernames/<username>/count', UsernameCountView.as_view()),
+    path('usernames/<username:username>/count', UsernameCountView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from utils.converters import UsernameConverter
+from django.urls import register_converter
+
+register_converter(UsernameConverter, 'username')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
