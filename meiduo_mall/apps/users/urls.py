13466cc8d58f34a6c469c79a0de3295a89128874
wangjun-alt <2797094441@qq.com>
2022-11-29 11:20:21 +0800

    登录的实现

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.users.views import UsernameCountView
+from apps.users.views import UsernameCountView, RegisterView, LoginView
 
 urlpatterns = [
     # 判断用户名是否重复
     path('usernames/<username:username>/count', UsernameCountView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view())
 ]
